ddbd4c7b86e94bd13d4332b131532dd0687afb77
DrishyaDivakaran <drishyadivakaran000@gmail.com>
2022-02-03 01:38:09 +0530

    hai jenkin first commit and pratice

---
+++ src/test/java/com/crm/OrganiTest/OrganizationTest.java
@@ -0,0 +1,18 @@
+package com.crm.OrganiTest;
+
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	@Test
+	public void createOrganization()
+	{
+		System.out.println("excute create contact");
+	}
+	
+	@Test
+	public void modifyOrganization()
+	{
+		System.out.println("modify and excute");
+	}
+
+}
---
+++ src/test/java/com/crm/contactTest/ContactTest.java
@@ -0,0 +1,18 @@
+package com.crm.contactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public  void craeteContactTest()
+	{
+		System.out.println("execute create contact test");
+	}
+	
+	@Test
+	public void modiContactTest()
+	{
+		System.out.println("excute modification contact test");
+	}
+
+}
